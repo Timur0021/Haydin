cc40d0a1947e2a6dc50d3120b13994a23985d243
Timur0021 <tima99.stella@gmail.com>
2023-01-15 22:26:25 +0200

    New change

--- srt/database/db.php
+++ srt/database/db.php
@@ -7,8 +7,8 @@ require 'connect.php';
 	 print_r ($value);
 	 echo '</pre>';
  }
- 
- function checkError($query){
+ // Перевірка до бази данних
+ function dbCheckError($query){
 	$errInfo = $query->errorInfo();
 	if ($errInfo[0] !== PDO::ERR_NONE){
 		echo $errInfo[2];
@@ -16,14 +16,115 @@ require 'connect.php';
 	}
 	return true;
  }
- function selectAll($table){
+ // Запит на отримання данних з одної таблиці
+ function selectAll($table, $params = []){
 	global $pdo;
 	$sql = "SELECT * FROM $table";
+	
+	if(!empty($params)){
+		$i = 0;
+		foreach ($params as $key => $value){
+		  if(!is_numeric($value)){
+			  $value = "'".$value."'";
+		    }
+			if($i === 0){
+				$sql = $sql . " WHERE $key=$value";
+			}else{
+				$sql = $sql . " AND $key=$value";
+			}
+			$i++;
+		}
+	}
+	
 	$query = $pdo->prepare($sql);
 	$query->execute();
-	
+	dbCheckError($query);
 	return $query->fetchAll();
-   
+
 }
 
-pnt(selectAll(table:"users"));
+// Запит на отримання одного рядка з вибраної таблиці
+ function selectOne($table, $params = []){
+	global $pdo;
+	$sql = "SELECT * FROM $table";
+	
+	if(!empty($params)){
+		$i = 0;
+		
+		foreach ($params as $key => $value){
+		  
+		  if(!is_numeric($value)){
+			  $value = "'".$value."'";
+		    }
+			
+			if($i === 0){
+				$sql = $sql . " WHERE $key=$value";
+			}else{
+				$sql = $sql . " AND $key=$value";
+			}
+			$i++;
+		}
+	}
+	
+	$query = $pdo->prepare($sql);
+	$query->execute();
+	dbCheckError($query);
+	return $query->fetch();
+
+}
+
+// Запис в таблицю бази данних
+
+function insert($table, $params){
+	global $pdo;
+	$i = 0;
+	$coll = '';
+	$mask = '';
+	foreach ($params as $key => $value){
+		if ($i === 0){
+			$coll = $coll . "$key";
+		    $mask = $mask . "'" ."$value"."'";
+		}else{
+		$coll = $coll . ", $key";
+		$mask = $mask . ", '" . "$value"."'";
+		}
+		$i++;
+	}
+   
+   $sql = "INSERT INTO $table ($coll) VALUES ($mask)";
+	$query = $pdo->prepare($sql);
+	$query->execute();
+	dbCheckError($query);
+}
+
+// Оновлення рядка в таблиці
+
+function update($table, $id, $params){
+	global $pdo;
+	$i = 0;
+	$str = '';
+	foreach ($params as $key => $value){
+		if ($i === 0){
+		    $str = $str . $key . " = '" . $value ."'";
+		}else{
+		$str = $str .", " . $key . " = '" . $value ."'";
+		}
+		$i++;
+	}
+
+   $sql = "UPDATE $table SET $str WHERE id = $id";
+	$query = $pdo->prepare($sql);
+	$query->execute();
+	dbCheckError($query);
+}
+
+
+// Видалення рядка в таблиці
+
+function delete($table, $id){
+	global $pdo;
+   $sql = "DELETE FROM $table WHERE id = $id ";
+	$query = $pdo->prepare($sql);
+	$query->execute();
+	dbCheckError($query);
+}
